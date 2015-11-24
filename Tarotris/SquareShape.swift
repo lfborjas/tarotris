4c4432b455d8b151fd775210b0ff3fc680d74135
Luis Borjas <luisfborjas@gmail.com>
2015-11-24 00:06:25 -0600

    Copying the basic 7 shapes

---
+++ Tarotris/SquareShape.swift
@@ -0,0 +1,36 @@
+//
+//  SquareShape.swift
+//  Tarotris
+//
+//  Created by Luis Borjas on 11/23/15.
+//  Copyright © 2015 Luis Borjas. All rights reserved.
+//
+
+class SquareShape: Shape {
+    /*
+        |0*| 1|
+        |2| 3|
+    
+        * marks the row/column indicator for the shape
+    
+        this shape will not rotate
+    */
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.OneEighty: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.Ninety: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.TwoSeventy: [(0, 0), (1, 0), (0, 1), (1, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
